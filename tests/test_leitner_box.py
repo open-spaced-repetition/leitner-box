28b5993aeac1027a3e41bbb8c8a3f339abea03c2
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2024-10-21 14:26:22 -0500

    test on_fail='prev_card'

--- tests/test_leitner_box.py
+++ tests/test_leitner_box.py
@@ -72,3 +72,73 @@ class TestLeitnerBox:
         assert card.box == 2
         # card is also due next day because that's a day that box 2 is repeated
         assert card.due == datetime(2024, 1, 16, 0, 0, 0, 0)
+
+    def test_basic_review_schedule_with_on_fail_prev_box(self):
+
+        # create Leitner system at 2:30pm on Jan. 1, 2024
+        start_datetime = datetime(2024, 1, 1, 14, 30, 0, 0)
+        on_fail = 'prev_box'
+        scheduler = LeitnerScheduler(start_datetime=start_datetime, on_fail=on_fail)
+
+        # create new Card
+        card = Card()
+
+        assert card.box == 1
+        assert card.due is None
+
+        # fail the card 2:35pm on Jan. 1, 2024
+        rating = Rating.Fail
+        review_datetime = datetime(2024, 1, 1, 14, 35, 0, 0)
+        card, review_log = scheduler.review_card(card, rating, review_datetime)
+
+        assert card.box == 1
+        assert card.due == datetime(2024, 1, 2, 0, 0, 0, 0)
+
+        # pass the card on Jan. 2
+        rating = Rating.Pass
+        review_datetime = datetime(2024, 1, 2, 0, 0, 0, 0)
+        card, review_log = scheduler.review_card(card, rating, review_datetime)
+
+        assert card.box == 2
+        assert card.due == datetime(2024, 1, 4, 0, 0, 0, 0)
+
+        # attempt to pass the card on Jan. 3 when it is not due
+        rating = Rating.Pass
+        review_datetime = datetime(2024, 1, 3, 0, 0, 0, 0)
+        with pytest.raises(RuntimeError):
+            card, review_log = scheduler.review_card(card, rating, review_datetime)
+
+        # pass card on Jan. 4
+        rating = Rating.Pass
+        review_datetime = datetime(2024, 1, 4, 0, 0, 0, 0)
+        card, review_log = scheduler.review_card(card, rating, review_datetime)
+
+        assert card.box == 3
+        assert card.due == datetime(2024, 1, 7, 0, 0, 0, 0)
+
+        # pass card on Jan. 7
+        rating = Rating.Pass
+        review_datetime = datetime(2024, 1, 7, 0, 0, 0, 0)
+        card, review_log = scheduler.review_card(card, rating, review_datetime)
+
+        # card is still in box 3
+        assert card.box == 3
+        assert card.due == datetime(2024, 1, 14, 0, 0, 0, 0)
+
+        # fail card on Jan. 14
+        rating = Rating.Fail
+        review_datetime = datetime(2024, 1, 14, 0, 0, 0, 0)
+        card, review_log = scheduler.review_card(card, rating, review_datetime)
+
+        # card moves back to box 1
+        assert card.box == 2
+        assert card.due == datetime(2024, 1, 16, 0, 0, 0, 0)
+
+        rating = Rating.Pass
+        review_datetime = datetime(2024, 1, 16, 0, 0, 0, 0)
+
+        card, review_log = scheduler.review_card(card, rating, review_datetime)
+
+        assert card.box == 3
+        # card is also due next day because that's a day that box 2 is repeated
+        assert card.due == datetime(2024, 1, 21, 0, 0, 0, 0)
