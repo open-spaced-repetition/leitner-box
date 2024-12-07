11ebbe8ac125c25fe421ce7f8c98e9c29e267791
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2024-12-07 16:01:49 -0600

    lint with ruff

--- tests/test_leitner_box.py
+++ tests/test_leitner_box.py
@@ -370,8 +370,8 @@ class TestLeitnerBox:
         old_card = deepcopy(card)
 
         # card and scheduler are json-serializable
-        assert type(json.dumps(scheduler.to_dict())) == str
-        assert type(json.dumps(card.to_dict())) == str
+        assert type(json.dumps(scheduler.to_dict())) is str
+        assert type(json.dumps(card.to_dict())) is str
 
         # card can be serialized and de-serialized while remaining the same
         card_dict = card.to_dict()
@@ -391,7 +391,7 @@ class TestLeitnerBox:
         card, review_log = scheduler.review_card(card=card, rating=rating, review_duration=review_duration)
 
         # review log is json-serializable
-        assert type(json.dumps(review_log.to_dict())) == str
+        assert type(json.dumps(review_log.to_dict())) is str
 
         # the new reviewed card can be serialized and de-serialized while remaining the same
         card_dict = card.to_dict()
